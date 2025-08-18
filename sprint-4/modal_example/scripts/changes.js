b7431dca59ba61fa39699e662e6d8955fb85e9ac
Guilherme de Souza <guilherme.d.souza@tripleteam.com>
2025-08-18 17:27:42 -0300

    sprint 4: events explanation

--- sprint-4/modal_example/scripts/changes.js
+++ sprint-4/modal_example/scripts/changes.js
@@ -1,14 +1,25 @@
-const modalButton = document.querySelector(".modal__button");
+// when the user clicks -> eventListener
+// a button ->
+//  -> how I select an element?
+//   ->  using querySelector with id, class or tag
+// a modal, thats hidden, should be visible
+// -> CSS class
 
-modalButton.addEventListener("click", function () {
+// a button ->
+//  -> how I select an element?
+//   ->  using querySelector with id, class or tag
+const clickMeButton = document.querySelector(".modal__button");
+
+// when the user clicks (button) -> eventListener
+clickMeButton.addEventListener("click", function () {
   console.log("Button clicked!");
   // when button was clicked
   // add a class that shows the modal background and the modal
-  const modalBackground = document.getElementsByClassName("modal__background");
-  console.log(modalBackground[0]);
 
-  // ADD CLASS
-  modalBackground[0].classList.add("modal__background-visible");
+  // block__element-modifier
+  // modal-is_open
+  const modalBackground = document.querySelector(".modal__background");
+  modalBackground.classList.add("modal__background-visible");
 
   const modalPost = document.querySelector(".modal-post");
   console.log(modalPost);
@@ -16,16 +27,20 @@ modalButton.addEventListener("click", function () {
   modalPost.classList.add("modal-visible");
 });
 
-const buttonCloseModal = document.getElementById("close");
-console.log(buttonCloseModal);
+// when the user click to close the modal
+// in a button
+// the modal should be hidden again
 
-// when someone click the close button in the modal
-// make the modal and the background disappear
-buttonCloseModal.addEventListener("click", function () {
-  // remove the modal background
-  const modalBackground = document.getElementsByClassName("modal__background");
-  modalBackground[0].classList.remove("modal__background-visible");
-  // remove the modal
-  const modalPost = document.querySelector(".modal-post");
-  modalPost.classList.remove("modal-visible");
-});
+// const buttonCloseModal = document.getElementById("close");
+// console.log(buttonCloseModal);
+
+// // when someone click the close button in the modal
+// // make the modal and the background disappear
+// buttonCloseModal.addEventListener("click", function () {
+//   // remove the modal background
+//   const modalBackground = document.getElementsByClassName("modal__background");
+//   modalBackground[0].classList.remove("modal__background-visible");
+//   // remove the modal
+//   const modalPost = document.querySelector(".modal-post");
+//   modalPost.classList.remove("modal-visible");
+// });
