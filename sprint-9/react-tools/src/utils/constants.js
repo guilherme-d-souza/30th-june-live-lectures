178d97f3e8e1884ec426adb17154180aa32fd5c3
Guilherme de Souza <guilherme.d.souza@tripleteam.com>
2025-09-16 16:55:03 -0300

    sprint 9: react tools part 2

---
+++ sprint-9/react-tools/src/utils/constants.js
@@ -0,0 +1,31 @@
+import rioDeJaneiroImg from "../assets/image.png";
+
+export const cards = [
+  {
+    imageSrc:
+      "https://plus.unsplash.com/premium_photo-1661868926397-0083f0503c07?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageAlt: "Soccer player with a ball",
+    title: "Soccer",
+    description: "I love soccer since I was a kid, was my favorite hobby!",
+  },
+  {
+    imageSrc: rioDeJaneiroImg,
+    imageAlt: "Rio de Janeiro",
+    title: "Rio de Janeiro",
+    description: "I love Rio!!",
+  },
+  {
+    imageSrc:
+      "https://images.unsplash.com/photo-1606733847546-db8546099013?q=80&w=2772&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageAlt: "Boardgame Catan being played",
+    title: "Boardgame",
+    description: "I play boardgame every time that I can!",
+  },
+  {
+    imageSrc:
+      "https://images.unsplash.com/photo-1606733847546-db8546099013?q=80&w=2772&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    imageAlt: "Boardgame Catan being played",
+    title: "Boardgame",
+    description: "I play boardgame every time that I can!",
+  },
+];
